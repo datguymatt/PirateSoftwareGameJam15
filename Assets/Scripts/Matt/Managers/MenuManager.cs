61b9cbad310c812b373cc6f9b77667074fb199fc
datguymatt <128839182+datguymatt@users.noreply.github.com>
2024-07-22 14:12:26 -0700

    added dotween packages, and copied in scripts and prefabs from an old project

---
+++ Assets/Scripts/Matt/Managers/MenuManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public void Update()
+    {
+        
+    }
+    //use these methods to be called when buttons are pushed
+
+    public void PressAnyKeyStart()
+    {
+        //disable the 
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+
+    }
+}
