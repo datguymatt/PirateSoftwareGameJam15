7c46f2f2037ba74b1ffc7f0f4529a7a99f0cb033
datguymatt <128839182+datguymatt@users.noreply.github.com>
2024-07-22 13:34:38 -0700

    Added TMP package, started on main menu
    Added TMP package, started on main menu

---
+++ Assets/Scripts/Matt/MenuManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    //use these methods to be called when buttons are pushed
+}
